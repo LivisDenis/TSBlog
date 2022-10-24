5eb4d9efcef4f470280822462f436d27cd257b62
LivisDenis <deniskachumachenko1@gmail.com>
2022-10-24 05:50:50 +0300

    add cors

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import checkAuth from "./middleware/checkAuth.js";
 import UserController from "./controllers/UserController.js";
 import PostController from "./controllers/PostController.js";
 import multer from "multer";
+import cors from "cors";
 import handleValidationErrors from "./utils/handleValidationErrors.js";
 dotenv.config()
 
@@ -22,6 +23,7 @@ const storage = multer.diskStorage({
 const upload = multer({storage})
 
 app.use(express.json())
+app.use(cors())
 app.use('/uploads', express.static('uploads'))
 
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login)
